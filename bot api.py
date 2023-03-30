4f9fe2be992e8fb3ea22ebe1a2c851426a9132fc
VSOV-git <metroexodus3@yandex.ru>
2023-03-31 01:09:55 +0300

    New language system v1.0

--- bot api.py
+++
@@ -1,1 +0,0 @@
-bot = telebot.TeleBot("5828330086:AAEYkzPskI5TTcb2yacFn165erjgeM6l21Q")
--- botTg.py
+++ botTg.py
@@ -3,6 +3,126 @@ import time
 import datetime
 import subprocess
 
+##dicts
+dict_start = {
+    "start": {"ru": ">>Процесс сканирования запущен<<",
+              "en": ">>Scanning system<<"
+              },
+    "while": {
+                "yes": {
+                    "ru": "Процесс завершён!",
+                    "en": "Process terminate"
+                },
+                "no": {
+                    "ru": "Процесс пропущен!",
+                    "en": "Process skipped"
+                }
+             },
+    "whait": {
+                "yes": {
+                    "ru": "Процесс завершён! По причине бездействия!",
+                    "en": "Process terminate while inaction"
+                },
+                "no": {
+                    "ru": "Процесс пропущен! По причине бездействия!",
+                    "en": "Process skipped while inaction"
+                }
+    }
+}
+dict_stop = {
+    "ru": "Команда /stop выполнена\nДождитесь надписи >>end<<",
+    "en": "Command /stop complete\nWait message >>end<<"
+}
+dict_help = {
+    "ru": 'AlertBot-бот для отслеживания процессов на вашем ПК\n'
+          'Список команд бота\n\n'
+          '/help - выводит список команд\n\n'
+          '/start - начинает сканирование системы и работу программу\n\n'
+          '/stop - останавливает сканирование системы и работу программы\n\n'
+          '/yes - останавливает процесс на ПК\n\n'
+          '/no - пропускает процесс на ПК \n\n'
+          '/wait (yes/no) - какую команду выполнить при бездействии\n\n'
+          '/addproc (yes/no) - добавляет дополнительный процесс в файл поиска \n\n'
+          '/delproc (yes/no) - удаляет процесс из файла поиска  \n\n'
+          '/prproc - вывести все процессы из файла killApp.txt\n\n',
+
+    "en": 'AlertBot is a bot to monitor processes on your PC\n'
+          'List of bot commands\n\n'
+          '/help - displays a list of commands\n\n'
+          '/start - starts scanning the system and running the program\n\n'
+          '/stop - Stops system scan and program\n\n'
+          '/yes - Stop process on PC\n\n'
+          '/no - skip process on PC\n\n'
+          '/wait (yes/no) - what command to execute when idle\n\n'
+          '/addproc (yes/no) - Adds an extra process to the search file\n\n'
+          '/delproc (yes/no) - remove process from search file\n\n'
+          '/prproc - print process in killApp.txt file\n\n'
+
+}
+dict_wait = {
+    "start": {
+                "ru": "Какую команду выполнить при длительном ожидании?\n\n" 
+                      "На данный момент активна команда /{}\n\n"
+                      "Если хотите поменять, то напишите команду\n\n"
+                      "/yes - чтобы процесс сам завершался при бездействии\n\n"
+                      "/no - чтобы процесс не завершался при бездействии",
+
+                "en": "What command to execute when waiting for a long time?\n\n" 
+                      "At that moment it's /{}\n\n"
+                      "If u want to change it\n\n"
+                      "/yes - the process itself terminates when idle\n\n"
+                      "/no - the process itself continue when idle"
+             },
+    "inside": {
+                "ru": "Вы поменяли команду ожидания на /{}",
+
+                "en": "You change command on /{}"
+              }
+
+}
+dict_addproc = {
+    "start": {"ru":f"Введите команды\n\n"
+                   f"/yes - для добавления процесса\n\n"
+                   f"/no - для завершения",
+              "en": f"Send command\n\n"
+                    f"/yes - for add proces\n\n"
+                    f"/no - for break"
+              },
+    "exept": {
+                "ru": ">>Нельзя добавить пустоту<<",
+                "en": ">>u cant add None<<"
+             },
+    "while": {
+                "ru": "Вы завершили добавление процесса",
+                "en": "You break adding the process"
+             },
+    "answ": {
+                "ru": 'Процесс "{}.exe" добавлен',
+                "en": 'Process "{}.exe" added'
+            }
+}
+dict_delproc = {
+    "start": {"ru": f"Введите команды\n\n"
+                    f"/yes - для удаления процесса\n\n"
+                    f"/no - для завершения",
+              "en": f"Send command\n\n"
+                    f"/yes - for delete proces\n\n"
+                    f"/no - for break"
+              },
+    "exept": {
+                "ru": ">>Процесса с таким индексом нет<<",
+                "en": ">>there is no process with this index<<"
+             },
+    "while": {
+                "ru": "Вы завершили удаление",
+                "en": "Process was deleted"
+             },
+    "answ": {
+                "ru": "Процесс {} удален",
+                "en": "Process {} deleted"
+            }
+}
+
 
 def file_write(text, filename="logconf.txt", rej="w", encod="utf8") -> bool:
     # writing data in file
@@ -97,7 +217,7 @@ def convert_type(lst: list) -> list:
     return m
 
 
-bot = telebot.TeleBot("5828330086:AAEYkzPskI5TTcb2yacFn165erjgeM6l21Q")
+bot = telebot.TeleBot("5828330086:AAGCyMebjBxrzFDzKXOhJYhG3c__LjWEkCA")
 blacklist = file_read("blacklist.txt")
 Lcheck_app = file_read(f"killApp.txt")
 # check_app = convert_type(kill_app)
@@ -136,6 +256,7 @@ def pid_kill(pid_id: str):
 
 
 def get_proclist():
+    global Lpid_tasklist
     Pid_list_out = set()
     Lpid_tasklist = command_execution_write('cp866', 'TASKLIST /FO CSV /NH /FI "IMAGENAME ne svchost.exe"')
     for line in Lpid_tasklist:
@@ -155,21 +276,17 @@ def send_message(message):
     :return: bot.send_message
     """
     global Btg_bot_send, Btask_cmd, Bcommand_wait_cmd, Lpid_tasklist
-    # time_test = time.time()
     Btg_bot_send = True
     Btask_cmd = None
     Spid_procsess = set()
-    Snetstat_proc = set()
-    bot.send_message(message.chat.id, f">>Процесс сканирования запущен<<")
+    bot.send_message(message.chat.id, dict_start["start"][language])
     while Btg_bot_send:
         while len(Spid_procsess) >= 1:
             for PID in Spid_procsess:
                 PID = PID.replace('"', "")
                 Stext_out_bot = str(pid_inf(PID))
                 bot.send_message(message.chat.id, Stext_out_bot)
-                # bot.send_message(message.chat.id, str(time.time() - time_test))
                 time_in = time.time()
-                time_out = 0
                 while True:
                     time_out = time.time()
                     if not Btg_bot_send:
@@ -177,18 +294,18 @@ def send_message(message):
                     if (time_out - time_in) >= 15:
                         if Bcommand_wait_cmd:
                             pid_kill(PID)
-                            bot.send_message(message.chat.id, "Процесс завершён! По причине бездействия!")
+                            bot.send_message(message.chat.id, dict_start["whait"]["yes"][language])
                             break
                         if not Bcommand_wait_cmd:
-                            bot.send_message(message.chat.id, "Процесс пропущен! По причине бездействия!")
+                            bot.send_message(message.chat.id, dict_start["whait"]["no"][language])
                             break
                     else:
                         if Bgot_True is True:
                             pid_kill(PID)
-                            bot.send_message(message.chat.id, "Процесс завершён!")
+                            bot.send_message(message.chat.id, dict_start["while"]["yes"][language])
                             break
                         if Bgot_True is False:
-                            bot.send_message(message.chat.id, "Процесс пропущен!")
+                            bot.send_message(message.chat.id, dict_start["while"]["yes"][language])
                             break
                         if not Btg_bot_send:
                             break
@@ -197,11 +314,6 @@ def send_message(message):
 
         else:
             Spid_procsess = get_proclist()
-            # Lpid_tasklist = command_execution_write('cp866', 'TASKLIST /FO CSV /NH /FI "IMAGENAME ne svchost.exe"')
-            # for line in Lpid_tasklist:
-            #     lst = line.split(",")
-            #     if lst[0] in Lcheck_app:
-            #         Spid_procsess.add(lst[1])
 
     bot.send_message(message.chat.id, ">>end<<")
 
@@ -210,41 +322,28 @@ def send_message(message):
 def send_message(message):
     global Btg_bot_send
     Btg_bot_send = False
-    bot.send_message(message.chat.id, "Команда /stop выполнена\nДождитесь надписи >>end<<")
+    bot.send_message(message.chat.id, dict_stop[language])
 
 
 @bot.message_handler(commands=['help'])
 def send_message(message):
-    bot.send_message(message.chat.id, f'AlertBot-бот для отслеживания процессов на вашем ПК\n\n'
-                                      f'Список команд бота\n'
-                                      f'/help - выводит список команд\n\n'
-                                      f'/start - начинает сканирование системы и работу программу\n\n'
-                                      f'/stop - останавливает сканирование системы и работу программы\n\n'
-                                      f'/yes - останавливает процесс на ПК\n\n'
-                                      f'/no - пропускает процесс на ПК \n\n'
-                                      f'/wait (yes/no) - какую команду выполнить при бездействии\n\n'
-                                      f'/addproc (yes/no) - добавляет дополнительный процесс в файл поиска \n\n'
-                                      f'/delproc (yes/no) - удаляет процесс из файла поиска  \n\n')
+    bot.send_message(message.chat.id, dict_help[language])
 
 
 @bot.message_handler(commands=['wait'])
 def send_message_in(message):
     global Bgot_True,Bcommand_wait_cmd
-    bot.send_message(message.chat.id, f"Какую команду выполнить при длительном ожидании?\n\n"
-                                      f"На данный момент активна команда /{Bcommand_wait_cmd}\n\n"
-                                      f"Если хотите поменять, то напишите команду\n\n"
-                                      f"/yes - чтобы процесс сам завершался при бездействии\n\n"
-                                      f"/no - чтобы процесс не завершался при бездействии")
+    bot.send_message(message.chat.id, dict_wait["start"][language].format(Bcommand_wait_cmd))
     time_in = time.time()
     time_out = 0
     while (time_out - time_in) <= 10:
         if Bgot_True is True:
             Bcommand_wait_cmd = True
-            bot.send_message(message.chat.id, f"Вы поменяли команду ожидания на /{Bcommand_wait_cmd}")
+            bot.send_message(message.chat.id, dict_wait["inside"][language].format(Bcommand_wait_cmd))
             break
         if Bgot_True is False:
             Bcommand_wait_cmd = False
-            bot.send_message(message.chat.id, f"Вы поменяли команду ожидания на /{Bcommand_wait_cmd}")
+            bot.send_message(message.chat.id, dict_wait["inside"][language].format(Bcommand_wait_cmd))
             break
     bot.send_message(message.chat.id, f">>quit<<\n >>{send_message_in.__name__}<<")
 
@@ -276,85 +375,81 @@ def send_message_del(message):
 
 
 @bot.message_handler(commands=['addproc'])
-def send_message_del(message):
+def send_message_add(message):
     try:
         global Bgot_True
         Stext_mes = message.text.split(" ")[1]
-        bot.send_message(message.chat.id, f"Введите команды\n\n"
-                                          f"/yes - для добавления процесса\n\n"
-                                          f"/no - для завершения")
+        bot.send_message(message.chat.id, dict_addproc["start"][language])
         time_out_func = 0
         time_in_func = time.time()
         while (time_out_func - time_in_func) <= 20:
             time_out_func = time.time()
             if Bgot_True is True:
                 Lcheck_app.append(f'"{Stext_mes}.exe"')
-                bot.send_message(message.chat.id, f'Процесс "{Stext_mes}.exe" добавлен')
+                bot.send_message(message.chat.id, dict_addproc["answ"][language].format(Stext_mes))
                 break
             if Bgot_True is False:
-                bot.send_message(message.chat.id, "Вы завершили добавление")
-                break
-        bot.send_message(message.chat.id, f">>quit<<\n >>{send_message_del.__name__}<<")
-    except Exception as error:
-        if error.__class__.__name__ == "IndexError":
-            bot.send_message(message.chat.id, f">>IndexError: {str(error)}<<")
-            bot.send_message(message.chat.id, ">>Нельзя добавить пустоту<<")
-        else:
-            bot.send_message(message.chat.id, str(error))
-
-
-@bot.message_handler(commands=['delproc'])
-def send_message_add(message):
-    try:
-        global Bgot_True
-        Stext_mes = message.text.split(" ")[1]
-        bot.send_message(message.chat.id, str(Lcheck_app[int(Stext_mes)]))
-        bot.send_message(message.chat.id, f"Введите команды\n\n"
-                                          f"/yes - для удаления процесса\n\n"
-                                          f"/no - для завершения")
-        time_out_func = 0
-        time_in_func = time.time()
-        while (time_out_func - time_in_func) <= 20:
-            time_out_func = time.time()
-            if Bgot_True is True:
-                bot.send_message(message.chat.id, f"Процесс {Lcheck_app[int(Stext_mes)]} удален")
-                Lcheck_app.pop(int(Stext_mes))
-                break
-            if Bgot_True is False:
-                bot.send_message(message.chat.id, "Вы завершили удаление")
+                bot.send_message(message.chat.id, dict_addproc["while"][language])
                 break
         bot.send_message(message.chat.id, f">>quit<<\n >>{send_message_add.__name__}<<")
     except Exception as error:
         if error.__class__.__name__ == "IndexError":
             bot.send_message(message.chat.id, f">>IndexError: {str(error)}<<")
-            bot.send_message(message.chat.id, ">>Процесса с таким индексом нет<<")
+            bot.send_message(message.chat.id, dict_addproc["exept"][language])
+        else:
+            bot.send_message(message.chat.id, str(error))
+
+
+@bot.message_handler(commands=['delproc'])
+def send_message_del(message):
+    try:
+        global Bgot_True
+        Stext_mes = message.text.split(" ")[1]
+        bot.send_message(message.chat.id, str(Lcheck_app[int(Stext_mes)]))
+        bot.send_message(message.chat.id, dict_delproc["start"][language])
+        time_out_func = 0
+        time_in_func = time.time()
+        while (time_out_func - time_in_func) <= 20:
+            time_out_func = time.time()
+            if Bgot_True is True:
+                bot.send_message(message.chat.id, dict_delproc["answ"][language].format(Lcheck_app[int(Stext_mes)]))
+                Lcheck_app.pop(int(Stext_mes))
+                break
+            if Bgot_True is False:
+                bot.send_message(message.chat.id, dict_delproc["while"][language])
+                break
+        bot.send_message(message.chat.id, f">>quit<<\n >>{send_message_del.__name__}<<")
+    except Exception as error:
+        if error.__class__.__name__ == "IndexError":
+            bot.send_message(message.chat.id, f">>IndexError: {str(error)}<<")
+            bot.send_message(message.chat.id, dict_delproc["exept"][language])
         else:
             bot.send_message(message.chat.id, f"{error.__class__.__name__}: {str(error)}")
 
-#add a ful support
+
 @bot.message_handler(commands=['language'])
-def send_message_leng(message):
+def send_message_lang(message):
     global language
     time_in = time.time()
     if language == "ru":
         bot.send_message(message.chat.id, "Send /yes, if u want change a language to English"
                                           "\n\n Send /no to continue")
-    elif language == "eu":
+    elif language == "en":
         bot.send_message(message.chat.id, "Отправьте /yes, если хотите поменять язык на Русский"
-                                          "\n\n Отпавьте /no для продолжения")
+                                          "\n\n Отправьте /no для продолжения")
     while time.time() - time_in <= 10:
         if Bgot_True is True:
             if language == "ru":
-                language = "eu"
+                language = "en"
                 bot.send_message(message.chat.id, f"Language changed to {language}")
                 break
-            if language == "eu":
+            if language == "en":
                 language = "ru"
                 bot.send_message(message.chat.id, f"Язык поменян на  {language}")
                 break
         if Bgot_True is False:
             break
-    bot.send_message(message.chat.id, f">>{send_message_leng.__name__}<<")
+    bot.send_message(message.chat.id, f">>{send_message_lang.__name__}<<")
 
 
 bot.polling()
